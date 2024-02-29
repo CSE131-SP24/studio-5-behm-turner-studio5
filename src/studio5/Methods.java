e5d2185f040e732f3af6f5ed243542c9668d642d
gretchenannbehm-turner <gretchenannbehm-turner@localhost>
2024-02-29 11:16:08 -0600

    Finished studio 5

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -14,9 +14,8 @@ public class Methods {
 	 * @return the Euclidean distance between (x1,y1) and (x2,y2)
 	 */
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
-		double distance = 0;
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
-		
+		double distance = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 		return distance;
 	}
 
@@ -34,17 +33,20 @@ public class Methods {
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
-
+		StdDraw.setPenColor(StdDraw.BLUE);
+		StdDraw.filledCircle(x, y, 3.0*radius/4.0);
 		
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
-
+		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.filledCircle(x, y, 1.0*radius/2.0);
 		
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
-
+		StdDraw.setPenColor(StdDraw.YELLOW);
+		StdDraw.filledCircle(x, y, 1.0*radius/4.0);
 		
 	}
 
@@ -61,6 +63,16 @@ public class Methods {
 	 */
 	public static String substituteAll(String source, char target, String replacement) {
 		String result = "";
+		char [] charArray = source.toCharArray();
+		for (int i=0; i<charArray.length; i++) {
+			if (charArray[i] == target) {
+		 	result += replacement;
+			}
+			else {
+			result += charArray[i];
+			}
+		}
+	
 		// TODO: Finish this method
 		
 		return result;
@@ -75,7 +87,9 @@ public class Methods {
 	public static int arraySum(int[] values) {
 		int sum = 0;
 		// FIXME: Compute the sum of the values in an array
-		
+		for (int i=0; i< values.length; i++) {
+			sum+= values [i];
+		}
 		return sum;
 	}
 
